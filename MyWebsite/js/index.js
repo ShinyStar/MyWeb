dd82a1df0653e1a0670a7de320a0afdf194a0029
Silvia (Star) Barbero Rodriguez <silvi.shinystar@gmail.com>
2016-04-11 18:12:01 +0200

    Added Silk chibi

--- MyWebsite/js/index.js
+++ MyWebsite/js/index.js
@@ -1,6 +1,6 @@
 function randomimage() {
     var pic;
-	var n=Math.floor((Math.random() * 11) + 1); //Random from 1-10 included
+	var n=Math.floor((Math.random() * 12) + 1); //Random from 1-12 included
     pic = n
     document.getElementById('myImage').src = "../images/randomChibis/"+n.toString()+".png";
 }
@@ -29,16 +29,15 @@ function stars(){
 	function draw()
 	{
 		context.clearRect(0,0,width, height);
-        context.fillStyle = "rgba(255, 255, 255, 0.5)";
+        context.fillStyle = "rgba(255, 255, 255, 1)";
 		stars.forEach(function(s){
             //base_radius = 2 + Math.floor(radius * s.z * (0.5 + Math.sin(2 * Math.PI * (t % s.t) / s.t)));
-            s.r=Math.random()*1+0.1;
+            s.r=Math.random()*5+0.1;
 			context.moveTo(s.x, s.y);
 			context.arc(s.x, s.y, s.r, 0, Math.PI*2, true);
 		});
 		context.fill();
 		update();
 	}
-
- 	//setInterval(function(){t++;draw()}, 30); PETA MUCHOOO
+ 	setInterval(function(){t++;draw()}, 30); //PETA MUCHOOO
 }
