5b632d23b78583de729f2cf275a86775a23c1bca
Silvia (Star) Barbero Rodriguez <silvi.shinystar@gmail.com>
2016-04-12 21:43:53 +0200

    Paradog chibi

--- MyWebsite/js/index.js
+++ MyWebsite/js/index.js
@@ -1,6 +1,6 @@
 function randomimage() {
     var pic;
-	var n=Math.floor((Math.random() * 12) + 1); //Random from 1-12 included
+	var n=Math.floor((Math.random() * 13) + 1); //Random from 1-13 included
     pic = n
     document.getElementById('myImage').src = "../images/randomChibis/"+n.toString()+".png";
 }
@@ -31,13 +31,14 @@ function stars(){
 		context.clearRect(0,0,width, height);
         context.fillStyle = "rgba(255, 255, 255, 1)";
 		stars.forEach(function(s){
-            //base_radius = 2 + Math.floor(radius * s.z * (0.5 + Math.sin(2 * Math.PI * (t % s.t) / s.t)));
+            //s.r = 2 + Math.floor(radius * s.z * (0.5 + Math.sin(2 * Math.PI * (t % s.t) / s.t)));
             s.r=Math.random()*5+0.1;
-			context.moveTo(s.x, s.y);
+            s.x=Math.random()*width;
+            s.y=Math.random()*height;
 			context.arc(s.x, s.y, s.r, 0, Math.PI*2, true);
 		});
+        update();
 		context.fill();
-		update();
 	}
- 	setInterval(function(){t++;draw()}, 30); //PETA MUCHOOO
+ 	setInterval(function(){t++;draw()}, 1000);
 }
